b5568dee6fbc052a7d031a98b04f73b0be382c1c
wizzardo <wizzardobox@gmail.com>
2014-10-27 22:36:54 +0300

    improve environment

--- test/com/wizzardo/servlet/ServerTest.java
+++ test/com/wizzardo/servlet/ServerTest.java
@@ -8,8 +8,10 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.log.StdErrLog;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 
+import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -17,6 +19,7 @@ import java.util.Properties;
  * Date: 23.10.14
  */
 public class ServerTest {
+    protected final String DEFAULT_PATH = "/servlet/";
     protected CustomServlet servlet;
     private Server jetty;
     private HttpServer myServer;
@@ -32,7 +35,7 @@ public class ServerTest {
         jetty = new Server(jettyPort);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/servlet");
+        context.setContextPath(DEFAULT_PATH);
         jetty.setHandler(context);
 
         servlet = new CustomServlet();
@@ -51,15 +54,29 @@ public class ServerTest {
     public void tearDown() throws Exception {
         jetty.stop();
         servlet.clean();
+
+        myServer.stopEpoll();
+    }
+
+    protected com.wizzardo.tools.http.Request jettyRequest() {
+        return jettyRequest(DEFAULT_PATH);
+    }
+
+    protected com.wizzardo.tools.http.Request myRequest() {
+        return myRequest(DEFAULT_PATH);
     }
 
     protected com.wizzardo.tools.http.Request jettyRequest(String path) {
-        return HttpClient.createRequest("http://localhost:" + jettyPort + path)
-                .header("Connection", "Close");
+        return createRequest("http://localhost:" + jettyPort + path);
     }
 
     protected com.wizzardo.tools.http.Request myRequest(String path) {
-        return HttpClient.createRequest("http://localhost:" + myPort + path)
+        return createRequest("http://localhost:" + myPort + path);
+    }
+
+    protected com.wizzardo.tools.http.Request createRequest(String url) {
+        return HttpClient.createRequest(url)
+                .disableRedirects()
                 .header("Connection", "Close");
     }
 
@@ -67,5 +84,13 @@ public class ServerTest {
         response.headers().forEach((s, strings) -> {
             System.out.println(s + ": " + strings);
         });
+    }
+
+    protected void test(TestStrategy strategy) throws IOException {
+        Assert.assertEquals(strategy.exec(jettyRequest()), strategy.exec(myRequest()));
+    }
+
+    protected static interface TestStrategy<T> {
+        public T exec(com.wizzardo.tools.http.Request request) throws IOException;
     }
 }
