aa1df3a245c1299e5a166982cd15f4c89561c460
wizzardo <wizzardobox@gmail.com>
2015-01-21 21:41:21 +0300

    add filter test

--- src/test/java/com/wizzardo/servlet/WarTest.java
+++ src/test/java/com/wizzardo/servlet/WarTest.java
@@ -4,9 +4,9 @@ import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.servlet.*;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -166,6 +166,49 @@ public abstract class WarTest extends ServerTest {
                 }
 
                 resp.getWriter().write(param + ": " + getInitParameter(param));
+            }
+        }
+    }
+
+    public static class TestFilter extends WarTest {
+
+        @Override
+        protected void customizeWar(WarBuilder builder) {
+            servletPath = "/filtered";
+            builder.addClass(SimpleServlet.class);
+            builder.getWebXmlBuilder()
+                    .append(new WarBuilder.ServletMapping(SimpleServlet.class).url("/filtered"))
+                    .append(new WarBuilder.FilterMapping(SimpleFilter.class).url("/*"))
+            ;
+        }
+
+        @Test
+        public void test() throws IOException {
+            test(request -> request.get().asString());
+            Assert.assertEquals("<response>ok</response>", myRequest().get().asString());
+        }
+
+        public static class SimpleServlet extends HttpServlet {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.getWriter().write("ok");
+            }
+        }
+
+        public static class SimpleFilter implements Filter {
+            @Override
+            public void init(FilterConfig filterConfig) throws ServletException {
+            }
+
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+                response.getWriter().write("<response>");
+                chain.doFilter(request, response);
+                response.getWriter().write("</response>");
+            }
+
+            @Override
+            public void destroy() {
             }
         }
     }
