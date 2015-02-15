cecd954fa9390f4c90da77fe74794aed4e63fb31
wizzardo <wizzardobox@gmail.com>
2015-02-15 13:48:26 +0300

    improve test

--- src/test/java/com/wizzardo/servlet/war/TestDispatcher.java
+++ src/test/java/com/wizzardo/servlet/war/TestDispatcher.java
@@ -19,22 +19,32 @@ public class TestDispatcher extends WarTest {
     @Override
     protected void customizeWar(WarBuilder builder) {
         builder.addClass(ServletForward.class);
+        builder.addClass(Servlet1.class);
         builder.addClass(Servlet2.class);
         builder.addClass(ServletInclude.class);
         builder.addClass(Servlet3.class);
+        builder.addClass(Servlet4.class);
         builder.addClass(ServletIncludeByName.class);
+        builder.addClass(ServletForwardByName.class);
         builder.getWebXmlBuilder()
                 .append(new WarBuilder.ServletMapping(ServletForward.class).url("/test_forwarded"))
-                .append(new WarBuilder.ServletMapping(Servlet2.class).url("/forward"))
+                .append(new WarBuilder.ServletMapping(Servlet1.class).url("/forward"))
+                .append(new WarBuilder.ServletMapping(Servlet2.class).url("/forward_by_name"))
                 .append(new WarBuilder.ServletMapping(ServletInclude.class).url("/test_included"))
                 .append(new WarBuilder.ServletMapping(Servlet3.class).url("/include"))
+                .append(new WarBuilder.ServletMapping(Servlet4.class).url("/include_by_name"))
                 .append(new WarBuilder.ServletMapping(ServletIncludeByName.class).url("/test_included_name"))
+                .append(new WarBuilder.ServletMapping(ServletForwardByName.class).url("/test_forwarded_name"))
         ;
     }
 
     @Test
     public void test_dispatcher() throws IOException {
         servletPath = "/test_forwarded";
+        Assert.assertEquals("forwarded", jettyRequest().get().asString());
+        Assert.assertEquals("forwarded", myRequest().get().asString());
+
+        servletPath = "/test_forwarded_name";
         Assert.assertEquals("forwarded", jettyRequest().get().asString());
         Assert.assertEquals("forwarded", myRequest().get().asString());
 
@@ -56,11 +66,19 @@ public class TestDispatcher extends WarTest {
         }
     }
 
-    public static class Servlet2 extends HttpServlet {
+    public static class Servlet1 extends HttpServlet {
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             resp.getWriter().write("forwarded");
             Assert.assertEquals("/forward", req.getServletPath());
+        }
+    }
+
+    public static class Servlet2 extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.getWriter().write("forwarded");
+            Assert.assertEquals("/test_forwarded_name", req.getServletPath());
         }
     }
 
@@ -78,14 +96,30 @@ public class TestDispatcher extends WarTest {
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             resp.getWriter().write("included");
-            Assert.assertTrue(req.getServletPath().startsWith("/test_included"));
+            Assert.assertEquals("/test_included", req.getServletPath());
+        }
+    }
+
+    public static class Servlet4 extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.getWriter().write("included");
+            Assert.assertEquals("/test_included_name", req.getServletPath());
+        }
+    }
+
+    public static class ServletForwardByName extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            RequestDispatcher requestDispatcher = getServletContext().getNamedDispatcher("Servlet2");
+            requestDispatcher.forward(req, resp);
         }
     }
 
     public static class ServletIncludeByName extends HttpServlet {
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            RequestDispatcher requestDispatcher = getServletContext().getNamedDispatcher("Servlet3");
+            RequestDispatcher requestDispatcher = getServletContext().getNamedDispatcher("Servlet4");
             resp.getWriter().write("<b>");
             requestDispatcher.include(req, resp);
             resp.getWriter().write("</b>");
